9cca8b5be38247ef2260e7be060d3b83d5a40fb2
Abo_Med7at <abomed7at55@gmail.com>
2018-07-22 17:34:53 +0200

    PlayerService now runs as expected

--- app/src/main/java/com/andalus/abomed7at55/quranplayer/PlayerActivity.java
+++ app/src/main/java/com/andalus/abomed7at55/quranplayer/PlayerActivity.java
@@ -4,57 +4,55 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.widget.MediaController;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.andalus.abomed7at55.quranplayer.Objects.Sura;
 import com.andalus.abomed7at55.quranplayer.Utils.PlayerService;
 
-import java.io.IOException;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 //TODO Optimise and support savedInstantState
-public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener,
-        MediaController.MediaPlayerControl{
+public class PlayerActivity extends AppCompatActivity {
 
     private static final int LOADER_ID = 20;
 
     private static final String TAG = "AudioPlayer";
 
-    private MediaPlayer mMediaPlayer;
-    private MediaController mMediaController;
     private PlayerService mPlayerService;
 
-    private Handler handler = new Handler();
+    @BindView(R.id.btn_play)
+    Button btnPlay;
+    @BindView(R.id.btn_pause)
+    Button btnPause;
+    @BindView(R.id.btn_forward)
+    Button btnForward;
+    @BindView(R.id.btn_back)
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         //Toast.makeText(getBaseContext(),getIntent().getExtras().getString(Sura.STREAMING_SERVER_KEY),Toast.LENGTH_LONG).show();
-        startStreaming();
-    }
-
-    private void startStreaming(){
-        mMediaPlayer = new MediaPlayer();
-        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mMediaPlayer.setOnPreparedListener(this);
-        mMediaController = new MediaController(this);
-        new Player().execute(getIntent().getExtras().getString(Sura.STREAMING_SERVER_KEY));
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Intent i = new Intent(PlayerActivity.this, PlayerService.class);
-        bindService(i,mServiceConnection, Context.BIND_AUTO_CREATE);
+        i.putExtra(Sura.STREAMING_SERVER_KEY,getIntent().getExtras().getString(Sura.STREAMING_SERVER_KEY));
+        try {
+            bindService(i,mServiceConnection, Context.BIND_AUTO_CREATE);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private ServiceConnection mServiceConnection = new ServiceConnection() {
@@ -62,7 +60,6 @@ public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnP
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             PlayerService.LocalBinder binder = (PlayerService.LocalBinder) iBinder;
             mPlayerService = binder.getPlayerService();
-
         }
 
         @Override
@@ -74,119 +71,29 @@ public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnP
     @Override
     protected void onStop() {
         super.onStop();
-        unbindService(mServiceConnection);
+        //unbindService(mServiceConnection);
         /*mMediaController.hide();
         mMediaPlayer.stop();
         mMediaPlayer.release();*/
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mMediaController.show();
-        return false;
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mediaPlayer) {
-        Log.d(TAG, "onPrepared");
-        mMediaController.setMediaPlayer(this);
-        mMediaController.setAnchorView(findViewById(R.id.tv_anchor_view));
-
-        handler.post(new Runnable() {
-            public void run() {
-                mMediaController.setEnabled(true);
-                mMediaController.show();
-            }
-        });
-    }
-
-    @Override
-    public void start() {
-        mMediaPlayer.start();
+    @OnClick(R.id.btn_play)
+    void onPlayClicked(){
         mPlayerService.playMedia();
     }
 
-    @Override
-    public void pause() {
-        mMediaPlayer.pause();
+    @OnClick(R.id.btn_pause)
+    void onPauseClicked(){
+        mPlayerService.pauseMedia();
     }
 
-    @Override
-    public int getDuration() {
-        return mMediaPlayer.getDuration();
+    @OnClick(R.id.btn_back)
+    void onBackClicked(){
+        mPlayerService.backMedia();
     }
 
-    @Override
-    public int getCurrentPosition() {
-        return mMediaPlayer.getCurrentPosition();
-    }
-
-    @Override
-    public void seekTo(int i) {
-        mMediaPlayer.seekTo(i);
-    }
-
-    @Override
-    public boolean isPlaying() {
-        return mMediaPlayer.isPlaying();
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        return 0;
-    }
-
-    @Override
-    public boolean canPause() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return true;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return 0;
-    }
-
-
-    class Player extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            Boolean prepared;
-
-            try {
-                mMediaPlayer.setDataSource(strings[0]);
-                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        mediaPlayer.stop();
-                        mediaPlayer.reset();
-                    }
-                });
-
-                mMediaPlayer.prepare();
-                prepared = true;
-
-            } catch (Exception e) {
-                Log.e("MyAudioStreamingApp", e.getMessage());
-                prepared = false;
-            }
-
-            return prepared;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-            mMediaPlayer.start();
-        }
+    @OnClick(R.id.btn_forward)
+    void onForwardClicked(){
+        mPlayerService.forwardMedia();
     }
 }
--- app/src/main/java/com/andalus/abomed7at55/quranplayer/Utils/PlayerService.java
+++ app/src/main/java/com/andalus/abomed7at55/quranplayer/Utils/PlayerService.java
@@ -2,47 +2,75 @@ package com.andalus.abomed7at55.quranplayer.Utils;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+import android.util.Log;
+
+import com.andalus.abomed7at55.quranplayer.Objects.Sura;
+
+import java.io.IOException;
 
 public class PlayerService extends Service {
 
     private final IBinder mBinder = new LocalBinder();
+
     private static String mStreamingServer;
+    private MediaPlayer mMediaPlayer;
+
 
     public class LocalBinder extends Binder{
 
         public PlayerService getPlayerService(){
             return PlayerService.this;
         }
-
-        public void setStreamingServer(String streamingServer){
-            mStreamingServer = streamingServer;
-        }
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mStreamingServer = intent.getExtras().getString(Sura.STREAMING_SERVER_KEY);
+        try {
+            mMediaPlayer.setDataSource(mStreamingServer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+            }
+        });
+
+        try {
+            mMediaPlayer.prepare();
+            mMediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return mBinder;
     }
 
     public void playMedia(){
-        Toast.makeText(getApplicationContext(),"Played",Toast.LENGTH_SHORT).show();
+        mMediaPlayer.start();
     }
 
     public void pauseMedia(){
-
+        mMediaPlayer.pause();
     }
 
     public void forwardMedia(){
-
+        mMediaPlayer.seekTo(mMediaPlayer.getCurrentPosition()+2000);
     }
 
     public void backMedia(){
-
+        mMediaPlayer.seekTo(mMediaPlayer.getCurrentPosition()-2000);
     }
 
 }
